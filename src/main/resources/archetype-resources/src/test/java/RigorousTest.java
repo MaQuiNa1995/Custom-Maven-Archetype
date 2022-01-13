eab118434597ff6c86bb4eaf4db63ac07e75ea94
MaQuiNa1995 <maquina1995@gmail.com>
2022-01-13 02:08:40 +0100

    Añadido contenido de arquetipo funcional

---
+++ src/main/resources/archetype-resources/src/test/java/RigorousTest.java
@@ -0,0 +1,15 @@
+package ${package};
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RigorousTest {
+
+	@Test
+	void rigorousTest() {
+		Assertions.assertTrue(true);
+	}
+
+}
